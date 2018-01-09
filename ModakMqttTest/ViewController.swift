ae2ad1c0b52902c51e3cd7d3ad919ab8803ee395
sexyjoon <answns0486@gmail.com>
2018-01-09 16:49:12 +0900

    install carthage and test mqtt send

--- ModakMqttTest/ViewController.swift
+++ ModakMqttTest/ViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import RMQClient
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.send()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +21,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    func send() {
+        let conn = RMQConnection(uri: "amqp://modak.world:1883",
+                                delegate: RMQConnectionDelegateLogger())
+        conn.start()
+        let ch = conn.createChannel()
+        let x = ch.topic("topic_logs")
+        let msg = "hi"
+        x.publish(msg.data(using: .utf8))
+        print("Sent '\(msg)'")
+        conn.close()
+    }
+    
+    func receive() {
+        print("Attempting to connect to local RabbitMQ broker")
+        let conn = RMQConnection(delegate: RMQConnectionDelegateLogger())
+        conn.start()
+        let ch = conn.createChannel()
+        let q = ch.queue("hello")
+        print("Waiting for messages.")
+        q.subscribe({(_ message: RMQMessage) -> Void in
+            print("Received \(String(data: message.body, encoding: String.Encoding.utf8)!)")
+        })
+    }
 
 }
 
